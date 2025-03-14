dab0ff820f5121bf87884f02a7817ebfafc9e7f5
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2025-03-14 04:11:32 +0100

    Add test setup and ContextLoad integration test

---
+++ booklore-api/src/test/java/com/adityachandel/booklore/ContextLoadIT.java
@@ -0,0 +1,36 @@
+package com.adityachandel.booklore;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ContextLoadIT {
+
+    @Container
+    @SuppressWarnings("resource")
+    static final MariaDBContainer<?> mariadb = new MariaDBContainer<>("mariadb:11.4.5")
+            .withDatabaseName("booklore")
+            .withUsername("root")
+            .withPassword("password");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mariadb::getJdbcUrl);
+        registry.add("spring.datasource.username", mariadb::getUsername);
+        registry.add("spring.datasource.password", mariadb::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+        // Verifies application context loads successfully
+    }
+}
